44603eea827da100219a29f0a163373e47e7fd63
Shaquille Rivera <shaq.rivera@gmail.com>
2022-12-09 15:48:13 -0600

    Day 4 solution part 2

--- 4/camp-cleanup.js
+++ 4/camp-cleanup.js
@@ -6,12 +6,18 @@ const elfPairsStrings = []
 textByLine.forEach(pairString => elfPairsStrings.push(pairString.split(',')))
 const elfPairs = elfPairsStrings.map((pair) => pair.map(rangeString => rangeString.split('-').map(string => parseInt(string))))
 
-let overlappingPairs = 0
+let fullyOverlappingPairs = 0
+let partialOverlappingPairs = 0
 
 elfPairs.forEach(pair => {
     const biggerRange = ((pair[0][1] - pair[0][0]) >= (pair[1][1] - pair[1][0])) ? pair[0] : pair[1]
     const smallerRange = biggerRange === pair[0] ? pair[1] : pair[0]
-    if((biggerRange[0] <= smallerRange[0]) && (biggerRange[1] >= smallerRange[1])) overlappingPairs += 1
+    const lowerRange = biggerRange[0] <= smallerRange[0] ? biggerRange : smallerRange
+    const higherRange = lowerRange === biggerRange ? smallerRange : biggerRange
+
+    if((biggerRange[0] <= smallerRange[0]) && (biggerRange[1] >= smallerRange[1])) fullyOverlappingPairs += 1
+    if(lowerRange[1] >= higherRange[0]) partialOverlappingPairs += 1
 })
 
-console.log(`The number of overlapping pairs is ${overlappingPairs}`)
+console.log(`The number of fully overlapping pairs is ${fullyOverlappingPairs}`)
+console.log(`The number of partially overlapping pairs if ${partialOverlappingPairs}`)
